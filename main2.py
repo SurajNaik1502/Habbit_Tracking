a3aed092ba0de55f8fce581c5e1345a53b642d9f
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-04 16:57:23 +0530

    Setting up user account on pixela

---
+++ main2.py
@@ -0,0 +1,14 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params={
+    "token" : "fbksdjhbvfedbvienb324h4iefd",
+    "username": "suru",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
